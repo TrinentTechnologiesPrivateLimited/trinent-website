89915ccf31af6d6aa73c94cd9f2b8ae967ce65e4
TrinentTechnologiesPrivateLimited <titaniuminteractivelabs@gmail.com>
2026-03-30 13:10:25 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,15 @@
+export const metadata = {
+  title: "Trinent",
+  description: "Trinent Website",
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
