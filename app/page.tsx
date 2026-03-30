81e092dd42fe97a02108b717c3e6696638842f4d
TrinentTechnologiesPrivateLimited <titaniuminteractivelabs@gmail.com>
2026-03-30 13:08:15 +0530

    Refactor Home component in page.tsx
    Removed unused imports and simplified the component structure.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,6 @@
-"use client";
-import React from "react";
-
 export default function Home() {
   return (
-    <div style={{
-      background: "black",
-      color: "white",
-      height: "100vh",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center"
-    }}>
+    <div>
       <h1>Trinent Website Running</h1>
     </div>
   );
