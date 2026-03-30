372b6afacb74a3cec6c5313482760e8dec78bcee
TrinentTechnologiesPrivateLimited <titaniuminteractivelabs@gmail.com>
2026-03-30 12:54:39 +0530

    Add Home component with basic styling

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{background:"black", color:"white", height:"100vh", display:"flex", alignItems:"center", justifyContent:"center"}}>
+      <h1>Trinent Website Running</h1>
+    </div>
+  );
+}
